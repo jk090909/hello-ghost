15c9e2935e85b1cfc16e7acfe2078ee3f6a07a1b
jk090909 <78129173+jk090909@users.noreply.github.com>
2021-02-15 08:54:28 +0900

    Add files via upload

---
+++ hello-ghost 2/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    // gnb 열기
+    $('.btn-toggle').on('click', function(){
+      $('#gnb').addClass('show');
+      console.log('click');
+    });
+  
+    // gnb 닫기
+    $('.btn-close').on('click', function(){
+      $('#gnb').removeClass('show');
+    });
+
+});
